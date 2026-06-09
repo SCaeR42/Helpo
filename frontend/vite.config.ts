d99740544780ee9e35266249622a03df55693f78
SCaeR <mail@casper.pw>
2026-06-10 00:20:01 +0300

    feat: автообновления для Dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,36 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+    plugins: [
+        vue(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            // Актуальный способ задания алиаса @ для папки src
+            // '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
+    server: {
+        host: "::",
+        port: 5173,
+        watch: {
+            // Использовать опрос вместо системных событий
+            usePolling: true,
+            // Интервал опроса в мс (опционально)
+            interval: 100
+        },
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+            },
+        },
     },
-  },
 })
